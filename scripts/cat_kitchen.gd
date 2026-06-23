ed7d244154aa7d48cc8928ffd560212fd2d7793c
HipolitoThe3rd <54292812+HipolitoThe3rd@users.noreply.github.com>
2026-06-23 11:27:24 -0400

    ready to publish

--- scripts/cat_kitchen.gd
+++ scripts/cat_kitchen.gd
@@ -5,6 +5,7 @@ const SPEED = 30.0
 const JUMP_VELOCITY = -600.0
 
 @onready var sprite = $Sprite2D
+@onready var anim = $AnimationPlayer
 @onready var sfx_happy = $AudioCat/MeowHappy
 @onready var sfx_angry = $AudioCat/MeowAngry
 @onready var sfx_eat = $AudioCat/EatFish
@@ -13,23 +14,42 @@ var direction = 1
 var target: Fish # the fish that the Cat is targeting, if any
 
 func _physics_process(delta: float) -> void:	
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
 	
-	if direction == 1: # if moving right
-		sprite.flip_h = direction # flip sprite
-	elif direction == -1: # if moving left
-		sprite.flip_h = 0 # don't flip sprite
-	
-	if target == null:
-		pass
+	# handle full Cat 
+	if Global.hunger >= 100:
+		velocity.x = 0
 	else:
-		if target.is_dragging == false && velocity.y == 0:
-			velocity.y = JUMP_VELOCITY
+		# handle walking
+		if direction:
+			velocity.x = direction * SPEED
+		else:
+			velocity.x = move_toward(velocity.x, 0, SPEED)
+	
+		# jump when fish is dropped
+		if target == null:
+			pass
+		else:
+			if target.is_dragging == false && velocity.y == 0:
+				velocity.y = JUMP_VELOCITY
+				if anim.current_animation != "jump":
+					anim.play("jump")
+	
+	# handle sprite direction
+		if direction == 1: # if moving right
+			sprite.flip_h = direction # flip sprite
+		elif direction == -1: # if moving left
+			sprite.flip_h = 0 # don't flip sprite
 		
 	velocity.y += get_gravity().y * delta
+	
+	#print("velocity.y: ", velocity.y)
+	#print("velocity.x: ", velocity.x)
+	#print("anim.current_animation: ", anim.current_animation)
+	#print("hunger: ", Global.hunger)
+	if velocity.y > 16.5 and velocity.y < 300 and velocity.x != 0 and anim.current_animation != "walk" and Global.hunger < 100:
+		anim.play("walk")
+	
+
 	
 	move_and_slide()
 
