445c060677263ba54d60e79c099cf1945299f79c
HipolitoThe3rd <54292812+HipolitoThe3rd@users.noreply.github.com>
2026-06-26 21:11:16 -0400

    kitchen

--- scripts/global.gd
+++ scripts/global.gd
@@ -10,9 +10,9 @@ var cursor_shower_pour = load("res://sprites/cursors/cursor_shower-head_pressedd
 var mood = 90 # how happy the cat is; 0 is minimum and 100 is maximum, default 50
 
 # Cat mood contributors
-var hunger = 25 # 0 to 100, affects mood and behavior, default 30
+var hunger = 5 # 0 to 100, affects mood and behavior, default 30
 var energy = 80 # 0 to 100, decreases with activity, default 80
-var cleanliness = 95 # 0 to 100, decreases over time, affects mood, default 95
+var cleanliness = 55 # 0 to 100, decreases over time, affects mood, default 95
 var entertainment = 75 # 0 to 100, boredom level; too low means bored, default 50
 var affection = 15 # 0 to 100, needs player interaction to increase, default 40
 var bladder = 100 # 0 to 100, decreases over time/activity; at 0 cat soils itself and resets to 100, default 100
